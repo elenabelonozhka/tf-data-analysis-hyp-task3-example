7f161ec8fee87e176ed92c4af108913a6b24d552
elenabelonozhka <64587763+elenabelonozhka@users.noreply.github.com>
2023-04-13 21:36:00 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -5,7 +5,7 @@ import scipy.stats as st
 chat_id = 728846853 # Ваш chat ID, не меняйте название переменной
 
 def solution(x: np.array, y: np.array) -> bool: # Одна или две выборке на входе, заполняется исходя из условия
-    p = st.mannwhitneyu(x,y,alternative = 'greater')
+    p = st.mannwhitneyu(x,y,alternative = 'greater').pvalue
     if p < 0.09:
         return True
     else:
