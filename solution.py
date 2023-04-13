a5144e01180473253484ae5112e302926a5469e8
elenabelonozhka <64587763+elenabelonozhka@users.noreply.github.com>
2023-04-13 20:45:42 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,11 +1,12 @@
 import pandas as pd
 import numpy as np
+import scipy.stats as st
 
+chat_id = 728846853 # Ваш chat ID, не меняйте название переменной
 
-chat_id = 123456 # Ваш chat ID, не меняйте название переменной
-
-def solution(...) -> bool: # Одна или две выборке на входе, заполняется исходя из условия
-    # Измените код этой функции
-    # Это будет вашим решением
-    # Не меняйте название функции и её аргументы
-    return ... # Ваш ответ, True или False
+def solution(x: np.array, y: np.array) -> bool: # Одна или две выборке на входе, заполняется исходя из условия
+    p = st.mannwhitneyu(x,y,alternative = 'greater')
+    if p < 0.09:
+        return True
+    else:
+        return False
